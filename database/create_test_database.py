d4329834d82f7a598a85f3264fcaedd0e56d4129
[nolanwu1112] <nolanwu1112@gmail.com>
2017-08-05 08:58:52 -0400

    Made replica for vocab.db

--- database/create_test_database.py
+++ database/create_test_database.py
@@ -1,5 +1,19 @@
 """ Creating a test db before the real run"""
 import sqlite3
 
-SQL_ORIG = r"./vocab.py"
-SQL_REPL = r"./"
+SQL_ORIG = r"./vocab.db"
+SQL_REPL = r"./test.db"
+
+vocab_list = []
+with sqlite3.connect(SQL_ORIG) as connection:
+    c = connection.cursor()
+    c.execute("SELECT vocab FROM pho;")
+    raw_list = c.fetchall()
+
+vocab = [item[0] for item in raw_list]
+
+with sqlite3.connect(SQL_REPL) as connection:
+    c = connection.cursor()
+    for item in vocab:
+        c.execute("INSERT INTO pho (vocab) VALUES (?);", (item,))
+    
