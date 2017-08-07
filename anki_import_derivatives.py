4d2ace2e0f95b9c8e98eef3bbb1f137f0e26cc5b
[nolanwu1112] <nolanwu1112@gmail.com>
2017-08-07 14:02:44 -0400

    Successfully added a collection of related words to anki

--- anki_import_derivatives.py
+++ anki_import_derivatives.py
@@ -9,6 +9,8 @@ from aqt import mw
 from aqt.utils import showInfo
 from aqt.qt import *
 
+errorlog = []
+ERROR_LOG_PATH = r'./error.log'
 def adding_derivatives():
     """
     Obtaining a list of vocab from anki db
@@ -18,18 +20,30 @@ def adding_derivatives():
     count = 0
     total = len(ids_cards)
 
+#    for key, value in derivatives.items():
+#        if value is not None:
+#            derivatives[key] = value.decode("utf-8")
+
     for id_card in ids_cards:
         card = mw.col.getCard(id_card)
         note = card.note()
         vocab_anki = note['vocab']
         try:
-            note["derivative"] = der[vocab_anki].decode("utf-8")
+            if der[vocab_anki] is not None:
+                note["deri"] = der[vocab_anki].decode("utf-8")
+            else:
+                note["deri"] = "None"
         except KeyError:
-            pass
+            errorlog.append(vocab_anki + " ")
         note.flush()
         count = count + 1
     mw.reset()
 
+#    with open(ERROR_LOG_PATH, "w") as file:
+#        for word in errorlog:
+#            file.write(word)
+    error_info = ' '.join(errorlog)
+    showInfo(error_info)
     showInfo(r"Change completed: {}/{}".format(count, total))
 
 
